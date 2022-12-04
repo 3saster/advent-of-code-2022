663784fdc3e926fd4454474e8c847e394f0fdd56
3saster <paulbologea@hotmail.com>
2022-12-04 06:26:06 -0800

    Day 04

---
+++ Day 04/Solution.py
@@ -0,0 +1,19 @@
+def contained(range1,range2):
+    [bot1,top1] = [int(n) for n in range1.split('-')]
+    [bot2,top2] = [int(n) for n in range2.split('-')]
+    if (bot1 <= bot2 and top2 <= top1) or (bot2 <= bot1 and top1 <= top2):
+        return True
+    return False
+
+def overlap(range1,range2):
+    [bot1,top1] = [int(n) for n in range1.split('-')]
+    [bot2,top2] = [int(n) for n in range2.split('-')]
+    if (bot1 <= bot2 <= top1) or (bot1 <= top2 <= top1) or (bot2 <= bot1 <= top2) or (bot2 <= top1 <= top2):
+        return True
+    return False
+
+def Part1(data):
+    return len([d for d in data if contained(*d.split(','))])
+
+def Part2(data):
+    return len([d for d in data if overlap(*d.split(','))])
